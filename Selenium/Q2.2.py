c260601b23adb5bb00a66aac9514f2df64a102d6
Diana <diana.nevzorova17@gmail.com>
2022-10-08 22:15:01 +0300

    Selenium

---
+++ Selenium/Q2.2.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+
+options = Options()
+options.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+user = "guest"
+password = "welcome2qauto"
+driver.get("https://"+user+":"+password+"@"+"qauto2.forstudy.space/")
+# driver.get("https://qauto2.forstudy.space/")
+
+driver. find_element(By.CSS_SELECTOR, "a[href*='instagram']").send_keys("Selenium")
+
+driver.close()
---
+++ Selenium/Q4.2.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+
+options = Options()
+options.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+user = "guest"
+password = "welcome2qauto"
+driver.get("https://"+user+":"+password+"@"+"qauto2.forstudy.space/")
+# driver.get("https://qauto2.forstudy.space/")
+
+
+#element = driver.find_element(By.XPATH, "//button[contains(text(), 'Guest log in')]")
+driver. find_element(By.CSS_SELECTOR, "button[class$='guest']  ").send_keys("Selenium")
+
+driver.close()
